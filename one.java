4e2e5da9bba0cd0052d3e255cef68f3314ba4389
poornajp <129258646+poornajp@users.noreply.github.com>
2023-08-21 09:43:02 +0530

    Update one.java

--- one.java
+++ one.java
@@ -3,7 +3,7 @@ public class Main {
   public static void main(String[] args) {
     
     int first = 40;
-    int second = 20;
+    int second = 30;
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
